17470402c839ade8dcda15927b9d1a8df6316e5c
fretws <fretws@uw.edu>
2022-02-25 11:25:28 -0800

    Implement section with curved background that stretches

--- src/page/Landing/Landing.tsx
+++ src/page/Landing/Landing.tsx
@@ -1,11 +1,23 @@
 import React from "react"
-import styles from './landing.module.css'
+import styles from './Landing.module.css'
+import backgroundCurve from '../../res/images/landing-background-curve.svg'
 import Heading from '../../component/text/Heading'
 
 export default function Landing() {
   return (
-    <div>
-      <Heading type={'section'}>Landing</Heading>
+    <div className={styles.page}>
+      <section className={styles['with-background-color']}>
+        <Heading type={'section'}>Landing</Heading>
+      </section>
+      <img
+        src={backgroundCurve}
+        alt={"Decorative curve"}
+        aria-hidden={true}
+        className={styles['background-curve']}
+      />
     </div>
   )
 }
+/*
+className={styles.backgroundCurve}
+ */
