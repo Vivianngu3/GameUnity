e672b5d66f111a74f2267832405fae5566dec409
fretws <fretws@uw.edu>
2022-03-31 18:28:17 -0700

    Establish the game url route pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import {WITH_PERIPHERALS, ABOUT_US, GAME, RECAP} from './res/constants/url-endpoints'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import * as urls from './res/constants/url-endpoints'
+import {BrowserRouter, Route, Routes, Outlet} from 'react-router-dom'
 import Heading from './component/text/Heading'
 import Landing from './page/Landing/Landing'
 import WithPeripherals from './page/WithPeripherals/WithPeripherals'
@@ -11,12 +11,23 @@ export default function App() {
   <div className="App">
     <BrowserRouter>
       <Routes>
-        <Route path={WITH_PERIPHERALS} element={<WithPeripherals/>}>
+        <Route path={urls.WITH_PERIPHERALS} element={<WithPeripherals/>}>
           <Route index element={<Landing/>} />
-          <Route path={ABOUT_US} element={<Heading type={'section'}>About us!</Heading>} />
-          <Route path={RECAP} element={<Heading type={'section'}>Recap!</Heading>} />
+          <Route path={urls.ABOUT_US} element={<Heading type={'section'}>About us!</Heading>} />
+          <Route path={urls.RECAP} element={<Heading type={'section'}>Recap!</Heading>} />
         </Route>
-        <Route path={GAME} element={<Heading type={'section'}>Game Page!</Heading>} />
+        <Route path={urls.GAME} element={<Outlet />} >
+          <Route index element={<Heading type={'section'}>Start page!</Heading>} />
+          <Route path={urls.GAME_WELCOME} element={<Heading type={'section'}>Welcome</Heading>} />
+          <Route path={urls.CHOOSE_SEED} element={<Heading type={'section'}>Choose Seed</Heading>} />
+          <Route path={urls.CHOOSE_PLOT} element={<Heading type={'section'}>Choose Plot</Heading>} />
+          {/*
+          The onboarding and nurturing pages will have many different states to accommodate the
+          steps that make up each of these user flows.
+          */}
+          <Route path={urls.ONBOARDING} element={<Heading type={'section'}>Onboarding</Heading>} />
+          <Route path={urls.NURTURING} element={<Heading type={'section'}>Nurturing</Heading>} />
+        </Route>
       </Routes>
     </BrowserRouter>
     </div>
