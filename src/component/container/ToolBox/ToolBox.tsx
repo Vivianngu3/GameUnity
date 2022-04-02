dda6ae75766757dd5324ab160c3a55ac243af4cd
fretws <fretws@uw.edu>
2022-04-02 13:08:34 -0700

    Begin work on toolbox

--- src/component/container/ToolBox/ToolBox.tsx
+++ src/component/container/ToolBox/ToolBox.tsx
@@ -1,1 +1,25 @@
 import React from 'react';
+
+import scissors from '../../../res/images/scissors.svg'
+
+interface Tool {
+  name: string
+  onClick: () => {}
+  icon: any
+}
+
+export default function ToolBox() {
+  let tools = [
+    {name: 'scissors', onClick: () => {}, icon: scissors}
+  ]
+
+  tools.map(tool => {
+    <div>
+
+    </div>
+  })
+
+  return (
+
+  )
+}
