7634d8c6cbd72eab6a7be712b9d17c284607805e
Vivianngu3 <vn23@uw.edu>
2022-04-17 15:24:15 -0700

    Rel added

--- src/page/Recap/Recap.tsx
+++ src/page/Recap/Recap.tsx
@@ -106,7 +106,7 @@ export default function Recap () {
               <div className={styles['content']}>
                 <h2 className={styles['title']}>Chia Pets</h2>
                 <p> Chia Pets are figurines where the chia sprouts grow within a couple of weeks to look like the animal’s fur or hair. </p> 
-                <button> <a href='https://www.chia.com/' target="_blank"> Find More </a> </button> 
+                <button> <a href='https://www.chia.com/' target="_blank" rel="noreferrer"> Find More </a> </button> 
               </div>
           </div>
 
@@ -115,7 +115,7 @@ export default function Recap () {
               <div className={styles['content']}>
                 <h2 className={styles['title']}>Eastern Leaf</h2>
                 <p> Our little green trees are ready for adoption and looking for good homes.</p> 
-                <button> <a href='https://www.easternleaf.com/' target="_blank"> Find More </a> </button> 
+                <button> <a href='https://www.easternleaf.com/' target="_blank" rel="noreferrer"> Find More </a> </button> 
               </div>
           </div>
 
@@ -125,7 +125,7 @@ export default function Recap () {
                 <h2 className={styles['title']}>Plant Kits</h2>
                 <p> Nature’s Blossom all-in-one beginner’s seed starter kits are perfect for any age. 
                   Enjoy the rewarding feeling of watching your tomato garden grow. </p> 
-                <button> <a href='https://www.amazon.com/Natures-Blossom-Tomato-Gardening-Starter/dp/B06XJWQ8FM' target="_blank"> Find More </a> </button>
+                <button> <a href='https://www.amazon.com/Natures-Blossom-Tomato-Gardening-Starter/dp/B06XJWQ8FM' target="_blank" rel="noreferrer"> Find More </a> </button>
               </div>
           </div>
         
